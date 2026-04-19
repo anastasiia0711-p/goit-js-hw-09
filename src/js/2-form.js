6a37f444228f4cb113270cc45805fc82bee268f4
perepeluknastia1507@gmail.com <perepeluknastia1507@gmail.com>
2026-04-19 13:56:41 +0300

    solve tasks

---
+++ src/js/2-form.js
@@ -0,0 +1,54 @@
+
+let formData = { 
+    email: "", 
+    message: "" 
+};
+
+const form = document.querySelector('.feedback-form');
+const STORAGE_KEY = "feedback-form-state";
+
+populateForm();
+
+form.addEventListener('input', (event) => {
+    
+    formData[event.target.name] = event.target.value.trim();
+    
+    
+    localStorage.setItem(STORAGE_KEY, JSON.stringify(formData));
+});
+
+
+form.addEventListener('submit', (event) => {
+    event.preventDefault();
+
+    
+    if (formData.email === "" || formData.message === "") {
+        alert("Fill please all fields");
+        return;
+    }
+
+    
+    console.log("Submitted Data:", formData);
+
+    
+    localStorage.removeItem(STORAGE_KEY);
+    formData = { email: "", message: "" };
+    form.reset();
+});
+
+
+function populateForm() {
+    const savedData = localStorage.getItem(STORAGE_KEY);
+
+    if (savedData) {
+        
+        const parsedData = JSON.parse(savedData);
+        
+        
+        formData = parsedData;
+        
+        
+        form.elements.email.value = formData.email || "";
+        form.elements.message.value = formData.message || "";
+    }
+}
